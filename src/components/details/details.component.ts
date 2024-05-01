a8216aad5708a584b9b65a923c57b41d112f9919
weitzshaked <weitzshaked@gmail.com>
2024-05-01 21:12:06 +0300

    start-list-items

--- src/components/details/details.component.ts
+++ src/components/details/details.component.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+export interface RatingCategory {
+  name: string;
+  score: number;
+  percentage: number;
+  level: string;
+} 
+
 @Component({
     selector: 'details',
     standalone: true,
@@ -10,6 +17,12 @@ import { Router } from '@angular/router';
 })
 export class DetailsComponent {
   inputValue: string;
+
+  public categories: RatingCategory[] = [
+    { name: 'Factuality', score: 0.5, percentage: 50, level: 'Moderate' },
+    { name: 'Objectivity', score: 0.5, percentage: 50, level: 'Moderate' },
+    // ... other categories
+  ];
 
   constructor(private router: Router) {
     const navigationState = this.router.getCurrentNavigation()?.extras.state as {
